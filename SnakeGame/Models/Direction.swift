fb604d8a243c3315aee2e477e1eda0f498f50cac
Alyaatalaat28 <alyaatalaat205@gmail.com>
2026-04-25 14:35:35 +0300

    feat: add core enums direction, gamestate, snakeskin

--- SnakeGame/Models/Direction.swift
+++ SnakeGame/Models/Direction.swift
@@ -5,3 +5,18 @@
 //  Created by Alyaa on 4/25/26.
 //
 
+import Foundation
+
+enum Direction {
+    case up, down, left, right
+
+    // Prevent reversing into yourself
+    var opposite: Direction {
+        switch self {
+        case .up:    return .down
+        case .down:  return .up
+        case .left:  return .right
+        case .right: return .left
+        }
+    }
+}
--- SnakeGame/Models/GameState.swift
+++ SnakeGame/Models/GameState.swift
@@ -5,3 +5,11 @@
 //  Created by Alyaa on 4/25/26.
 //
 
+import Foundation
+
+enum GameState {
+    case home
+    case playing
+    case paused
+    case gameOver
+}
