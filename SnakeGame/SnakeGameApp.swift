06677e93a302aa5a60d134b52affc8c2f70fa978
Alyaatalaat28 <alyaatalaat205@gmail.com>
2026-04-25 14:27:17 +0300

    Initial Commit

---
+++ SnakeGame/SnakeGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnakeGameApp.swift
+//  SnakeGame
+//
+//  Created by Alyaa on 4/25/26.
+//
+
+import SwiftUI
+
+@main
+struct SnakeGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
